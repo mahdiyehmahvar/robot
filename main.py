39ec23cb77b88c0a47c7f00b483d9d7c5eeb6368
mahdiyehmahvar <98236284+mahdiyehmahvar@users.noreply.github.com>
2022-01-27 22:47:23 +0330

    Update main.py

--- main.py
+++ main.py
@@ -122,5 +122,5 @@ def read_json(filename='favoritelist.json'):
 
 
 write_json({})
-app.run(debug=True)
-#app.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
+#app.run(debug=True)
+app.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
